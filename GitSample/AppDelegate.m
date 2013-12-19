dfaaf32bb76a209278d643b7b2c5d22eb5c4e958
Harikrishna Pai <harikrishnapair@gmail.com>
2013-12-19 19:57:22 +0530

    Test

--- GitSample/AppDelegate.m
+++ GitSample/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Testingg
+    // Testingg again
     // Override point for customization after application launch.
     return YES;
 }
